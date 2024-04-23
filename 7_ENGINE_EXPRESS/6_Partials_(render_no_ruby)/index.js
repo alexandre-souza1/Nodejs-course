293a48f8b312adc8752be902ef959a9ca74be2b7
Cleiston Amaral <c.alexandresouza@yahoo.com>
2024-04-23 17:26:37 -0300

    node.js challenge 7/6

---
+++ 7_ENGINE_EXPRESS/6_Partials_(render_no_ruby)/index.js
@@ -0,0 +1,80 @@
+// partials sao como minitemplates, que precisam ser repetidos em diversos locais da nossa aplicacao;
+// precisamos realizar algumas modificacoes na implementacao do handlebars;
+// partials geralmente ficam em views/partials;
+// a sintaxe e: {{>partial}}
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+const hbs = exphbs.create({
+  partialsDir: ['views/partials'],
+})
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.get('/dashboard', (req, res) => {
+
+  const items = ["item a", "item b", "item c"]
+
+  res.render('dashboard', {items})
+})
+
+app.get('/post', (req, res) => {
+
+  const post = {
+    title: 'learn node.js',
+    category: 'javascript',
+    body: 'this article will help you to learn node.js',
+    comments: 4,
+  }
+
+  res.render('blogpost', {post})
+})
+
+app.get('/blog', (req, res) => {
+  const posts = [
+  {
+    title: 'learn node.js',
+    category: 'javascript',
+    body: 'this article will help you to learn node.js',
+    comments: 4,
+  },
+  {
+    title: 'learn PHP',
+    category: 'PHP',
+    body: 'this article will help you to learn PHP',
+    comments: 6,
+  },
+  {
+    title: 'learn Phyton',
+    category: 'Phyton',
+    body: 'this article will help you to learn Phyton',
+    comments: 9,
+  }
+]
+
+  res.render('blog', {posts})
+})
+
+app.get('/', (req, res) => {
+
+  const user = {
+    name: "Alex",
+    surname: "Souza",
+    age: "30"
+  }
+
+  const palavra = 'teste'
+
+  const auth = true
+
+  const approved = false
+
+  res.render('home', {user: user, palavra, auth, approved})
+});
+
+app.listen(3000, () => {
+  console.log('App is Working');
+});
